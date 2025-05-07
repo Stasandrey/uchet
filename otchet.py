2213496ead747de31e84dbfc60b9ba4425bc029b
stasandrey <staschenyuk.andrey@gmail.com>
2025-05-07 13:26:09 +0300

    Вроде готово

--- otchet.py
+++ otchet.py
@@ -42,6 +42,7 @@ def one_hour(now):
 def calculate():
     now = datetime.now()
     period = datetime.combine(now.date() - timedelta(days=1), time(00, 00, 00))
+    per = period.date().strftime('%d.%m.%Y')
     period = period - timedelta(hours=1)
     print(period)
     item = 0
@@ -53,40 +54,59 @@ def calculate():
     #
     for item in data:
         print(item)
+    return per
 
-def build_pdf():
+def build_pdf(per):
     xlist = [70, 150, 280, 410, 550]
     ylist = [750]
     for i in range(0,26):
         ylist.append(75 + i * 25)
     ylist.append(75)
     w, h = A4
-    c = canvas.Canvas("/home/andrey/homeassistant/www/data.pdf", pagesize=A4)
+    c = canvas.Canvas("/home/andrey/data.pdf", pagesize=A4)
+    c.drawString(250, 760, per)
 
     text1 = c.beginText(85, 708)
     text2 = c.beginText(190, 708)
     text3 = c.beginText(320, 708)
+    text4 = c.beginText(450,708)
     text1.setLeading(25)
     text2.setLeading(25)
     text3.setLeading(25)
+    text4.setLeading(25)
     text1.textLine('Time')
     text2.textLine('Pokazaniya')
     text3.textLine('Delta')
+    text4.textLine('Real')
+    s_delta = 0
+    s_real = 0
     for i in range(0,len(data)):
         text1.textLine(data[i]['time'])
         text2.textLine(str(data[i]['pok']))
         if i!=len(data) - 1:
-            text3.textLine(str(data[i+1]['delta']))
+            text3.textLine( str( data[i+1]['delta'] ) )
+            text4.textLine( str ( round( data[i+1]['delta'] * 60, 2) ) )
+            s_delta = s_delta + data[i+1]['delta']
+            s_real = s_real + data[i + 1]['delta'] * 60
+
+        else:
+            text3.textLine('')
+            text4.textLine('')
+
+    text3.textLine( str(s_delta) )
+    text4.textLine( str( round(s_real, 2) ) )
+
     c.drawText(text1)
     c.drawText(text2)
     c.drawText(text3)
+    c.drawText(text4)
     c.grid(xlist, ylist)
     c.showPage()
     c.save()
 
 def main():
-    calculate()
-    build_pdf()
+    per = calculate()
+    build_pdf(per)
 
 if __name__ == "__main__":
     main()
